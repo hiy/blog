696853c19a819d2d693d5149c20237fd848c1b28
Hiroshi Yamasaki <ymskhrs@gmail.com>
2022-12-18 21:27:03 +0900

    fix config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://hiy.github.io/",
-  base: '/blog',
+  outDir: './dist/blog',
   integrations: [
     tailwind({
       config: {
